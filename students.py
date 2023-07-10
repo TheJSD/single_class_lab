222bc84513260d275264c6b06852ea442d81623c
James Dickson <jamesstuartdickson@gmail.com>
2023-07-10 13:56:16 +0100

    created student class

---
+++ students.py
@@ -0,0 +1,6 @@
+class Student:
+
+    def __init__(self, name, cohort, favourite_language):
+        self.name = name
+        self.cohort = cohort
+        self.favourite_langauge = favourite_language
