e2e368180f887ebe5ea1619b7fb50fec103a628e
James Dickson <jamesstuartdickson@gmail.com>
2023-07-10 15:16:05 +0100

    added runner.py used to test the other files

---
+++ runner.py
@@ -0,0 +1,18 @@
+from students import *
+
+# student = Student("Steve", "E45")
+# print(student)
+
+# student.talk()
+
+# student.say_favourite_language("Python")
+
+from teams import *
+
+team1 = Team("TeamTesters", ["Paul", "Mike", "Dave", "Sara"], "Papa Smurf")
+
+print(team1.players)
+
+print(team1.has_player("Paul"))
+print(team1.has_player("Papa Smurf"))
+
